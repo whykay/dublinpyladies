744b9d892fa1407a3d03d3f945944e5a6440795c
whykay <whykay@gmail.com>
2013-11-11 18:40:13 +0000

    Redirecting to the meetup page

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return flask.redirect("http://www.meetup.com/PyLadiesDublin/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
